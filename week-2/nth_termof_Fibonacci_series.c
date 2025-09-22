89920959fc213d707d95270dc1a2f2d21095804d
siddharthasunkara52-ux <siddharthasunkara52@gmail.com>
2025-09-22 21:25:34 +0530

    Add files via upload

---
+++ week-2/nth_termof_Fibonacci_series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+long long fibonacci(int n) {
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("The %dth Fibonacci term is %lld\n", n, fibonacci(n));
+    return 0;
+}
